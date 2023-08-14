700daf3b88cecdefac46e628e034ade2c03b8e42
Paramananda Behera <72434793+parama-cool@users.noreply.github.com>
2023-08-14 20:28:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "My_ec2_instance"{
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "s3bucket_7hhj9"
+  bucket = "s3bucket_awsssss"
   acl    = "private"
 
   versioning = {
