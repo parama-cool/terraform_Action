5272b9a155c07ce5a0b5ed8af84510a995232219
Paramananda Behera <72434793+parama-cool@users.noreply.github.com>
2023-08-14 19:15:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,8 +20,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
+  ami = ami-0da59f1af71ea4ad2
   instance_type = "t2.micro"
+  key = "terraform_ac.pem"
 
 
   tags = {
