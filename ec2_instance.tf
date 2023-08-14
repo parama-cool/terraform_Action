090f1effb98e91da21378feef4413ff25e5a2a49
Paramananda Behera <72434793+parama-cool@users.noreply.github.com>
2023-08-14 15:21:28 +0530

    Update ec2_instance.tf

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -2,13 +2,15 @@ terraform{
         required_providers{
         aws={
         source = "hashicorp/aws"
-        version = "~>4.16"
+        version = "5.12.0"
 }
 }
 required_version = ">= 1.2.0"
 }
 provider "aws"{
         region = "us-east-1"
+        access_key = "AKIAZ4WS4SB6UHFDL6HW"
+        secret_key = "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"
 }
 
 resource "aws_instance" "My_ec2_instance"{
