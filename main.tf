4f32d0472c925141e56f73006684a4c06dc47329
Paramananda Behera <72434793+parama-cool@users.noreply.github.com>
2023-08-14 19:23:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
-  key_name = "terraform_ac.pem"
+  key_name      = "app-ssh-key"
 
 
   tags = {
